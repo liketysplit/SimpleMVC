7b0a5a06e515f2c48feb6b37d5c7b4a185135a72
liketysplit <rjboles2@gmail.com>
2019-01-28 14:02:03 -0500

    Added: Folder structure, Models, Views, and Basic Controllers

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/simpleMVC');
+  // Site Name
+  define('SITENAME', 'SimpleMVC');
+
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '123456');
+  define('DB_NAME', 'test');
