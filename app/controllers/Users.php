26cd735c09790877c6a59d3aee2b54db014d7710
liketysplit <rjboles2@gmail.com>
2019-01-28 15:57:56 -0500

    Added: Form Validation

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -5,8 +5,56 @@
     }
     
     public function register(){
+
+       
         
-        if($_SERVER['REQUEST_METHOD'] != 'POST'){
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+            $data = [
+                'name' => trim($_POST['name']),
+                'email' => trim($_POST['email']),
+                'password1' => trim($_POST['password1']),
+                'password2' => trim($_POST['password2']),
+                'error_name' => '',
+                'error_email' => '',
+                'error_password1' => '',
+                'error_password2' => ''
+            ];
+
+            if(empty($data['name'])){
+                $data['error_name'] = 'Please enter your name';
+            }
+            if(empty($data['email'])){
+                $data['error_email'] = 'Please enter your email';
+            }
+            if(empty($data['password1'])){
+                $data['error_password1'] = 'Please enter a password';
+            }else{
+                if(strlen($data['password1']) < 8){
+                    $data['error_password1'] = 'Password must be at least 8 characters';
+                }elseif ( $data['password1'] != $data['password2']){
+                    $data['error_password1'] = 'Please enter matching passwords';
+                }
+            }
+
+
+            if(empty($data['password2'])){
+                $data['error_password2'] = 'Please enter a password';
+            }else{
+                if(strlen($data['password2']) < 8){
+                    $data['error_password2'] = 'Password must be at least 8 characters';
+                }elseif ( $data['password1'] != $data['password2']){
+                    $data['error_password2'] = 'Please enter matching passwords';
+                }
+            }
+
+            if(!empty($data['error_name']) || !empty($data['error_email']) || !empty($data['error_password1']) || !empty($data['error_password2'])){
+                $this->view('users/register', $data);
+            }else{
+                die('TEMP');            
+            }
+
+        }else{
             $data = [
                 'name' => '',
                 'email' => '',
@@ -19,7 +67,6 @@
             ];
 
             $this->view('users/register', $data);
-        }else{
 
         }
      
@@ -37,7 +84,28 @@
 
             $this->view('users/login', $data);
         }else{
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+            $data = [
+                'email' => trim($_POST['email']),
+                'password1' => trim($_POST['password1']),
+                'error_email' => '',
+                'error_password1' => '',
+            ];
 
+            if(empty($data['email'])){
+                $data['error_email'] = 'Please enter your email';
+            }
+            if(empty($data['password1'])){
+                $data['error_password1'] = 'Please enter a password';
+            }else{
+
+            }
+
+            if(!empty($data['error_email']) || !empty($data['error_password1'])){
+                $this->view('users/login', $data);
+            }else{
+                die('TEMP');            
+            }
         }
         
     }
