8f274e1fd92bfb1a33e50ee0c0043ef9f707acd2
liketysplit <rjboles2@gmail.com>
2019-01-28 21:18:30 -0500

    Addded: Username to NavBar

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -24,8 +24,11 @@
         <ul class="navbar-nav ml-auto">
           <?php if(isset($_SESSION['user_id'])): ?>
           <li class="nav-item">
+              <a class="nav-link" href="#"><?php echo $_SESSION['user_name'];?></a>
+          </li>
+          <li class="nav-item">
               <a class="nav-link" href="<?php echo URLROOT; ?>/users/logout">Logout</a>
-            </li>
+          </li>
           <?php else : ?>
             <li class="nav-item">
               <a class="nav-link" href="<?php echo URLROOT; ?>/users/register">Register</a>
